1519ba6601d58a7255f0bfb076765f88958281d0
vnayak-bhatt <vinayak.bhatt@habilelabs.io>
2019-11-14 17:20:29 +0530

    log patch

--- src/log-object-entries.jsx
+++ src/log-object-entries.jsx
@@ -4,20 +4,20 @@ export default class record extends React.Component {
     constructor(props) {
         super(props);
         this.state={
-            firstName:   ''
+            value:   ''
         };
         console.log({props}, props.type);
         this.handleChange = this.handleChange.bind(this);
     };
 
     sendData = (value, type) => {
-        this.props.parentCallback(this.state.firstName, type);
+        this.props.parentCallback(this.state.value, type);
 
     };
     handleChange(event , param){
-            console.log({event})
-            this.setState({firstName: event.target.value});
-            this.sendData(this.state.firstName,this.props.type);
+            console.log({event}, 'handlechange triggered at input from child component')
+            this.setState({value: event.target.value});
+            this.sendData(this.state.value,this.props.type);
 
     }
     render() {
@@ -25,7 +25,7 @@ export default class record extends React.Component {
             <React.Fragment>
                 <div className={"log-input"}>
                     <p>{this.props.type}</p>
-                    <input type="text" value={this.state.firstName} onChange={(evt) => this.handleChange(evt)}/>
+                    <input type="text" value={this.state.value} onChange={(evt) => this.handleChange(evt)}/>
                 </div>
             </React.Fragment>);
     }
--- src/log.jsx
+++ src/log.jsx
@@ -2,57 +2,52 @@ import React from 'react';
 import Record from './log-object-entries'
 
 export default class Log extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             firstName: '',
             lastName: '',
             age: undefined,
         };
 
-        this.handleChangeFirstName = this.handleChangeFirstName.bind(this);
         this.fireLog = this.fireLog.bind(this);
-        this.makePerson = this.makePerson.bind(this);
+        this.fireLogStyle = this.fireLogStyle.bind(this);
     }
-    handleChangeFirstName(event , param){
-        console.log(event, param)
-        this.setState({person: {firstName: event.target.value}});
-    }
-    makePerson(type){
+
+    fireLog() {
         let person = {};
-        person.age = this.state.age;
         person.firstName = this.state.firstName;
         person.lastName = this.state.lastName;
-        type==='{}' ? this.fireLog(person): this.fireLog(person,'style');
-    }
-    fireLog(person, style){
-        style? console.log('style %c %s'):
         console.log({person});
     }
+
+    fireLogStyle() {
+        let stylistPerson = {};
+        stylistPerson.firstName = this.state.firstName;
+        stylistPerson.lastName = this.state.lastName;
+        console.log( '%c My name is %s %s', 'color: red; background-color: black; font-weight: bold',stylistPerson.firstName, stylistPerson.lastName);
+    }
+
     callbackFunction = (childData, type) => {
-        console.log({childData}, {type})
-        type==='firstName' ?          this.setState({firstName: childData}):
-            type==='lastName' ?         this.setState({lastName: childData}):
+        console.log({childData}, {type}, 'data received from child component');
+        type === 'firstName' ? this.setState({firstName: childData}) :
+            type === 'lastName' ? this.setState({lastName: childData}) :
                 this.setState({age: childData});
         console.log('this.state', this.state);
     };
-        // this.setState({{person: {firstName: event.target.value}: childData})
     render() {
         return (
             <React.Fragment>
                 <h1>Console.log</h1>
                 <h2>object person;</h2>
-                <div  className={"log-input"}>
-                    <p>person.firstName</p>
-                    {/*<input type="text" value={this.state.person.firstName} onChange= />*/}
+                <div className={"log-input"}>
                 </div>
-                    <Record parentCallback={this.callbackFunction} type="firstName"/>
-                    <Record parentCallback={this.callbackFunction} type="lastName"/>
-                    <Record parentCallback={this.callbackFunction} type="age"/>
-                {this.state.firstName}'esa'{this.state.lastName}'dsa'{this.state.age }
-                <button onClick={this.makePerson('{}')}>Click to fire log</button>
-                <button onClick={this.makePerson('style')}>style</button>
-                </React.Fragment>
+                <Record parentCallback={this.callbackFunction} type="firstName"/>
+                <Record parentCallback={this.callbackFunction} type="lastName"/>
+                {/*<Record parentCallback={this.callbackFunction} type="age"/>*/}
+                <button onClick={this.fireLog}>click to fire log</button>
+                <button onClick={this.fireLogStyle}>style</button>
+            </React.Fragment>
         )
     }
 }
