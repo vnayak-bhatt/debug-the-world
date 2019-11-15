26206adcf7b4f80cfc03bf8e1581cb00153f30f0
vnayak-bhatt <vinayak.bhatt@habilelabs.io>
2019-11-15 14:00:21 +0530

    time component included

--- src/App.js
+++ src/App.js
@@ -1,58 +1,64 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Presentation from './presentation'
 import Log from './log'
 import TableAssert from './table-assert'
+import Time from './time'
 
 class App extends React.Component {
 
-  render(){
-      return (
-          <div className="wrapper">
-            <Router>
-              <div className="App">
-                  <nav>
-                      <ul>
-                          <li >
-                              <Link to="/home">Home</Link>
-                          </li>
-                          <li >
-                              <Link to="/log">Log</Link>
-                          </li>
-                          <li >
-                              <Link to="/table-assert">Table|Assert</Link>
-                          </li>
-                          <li >
-                              <Link to="/todoclass">Todo</Link>
-                          </li>
-                      </ul>
-                  </nav>
-                  <header className="App-header">
-                      <Presentation />
-                      <Switch>
-                      <Route path="/home">
-                      </Route>
-                      <Route path="/" exact={true}>
-                          <Presentation />
-                      </Route>
-                      <Route path="/log">
-                          <Log />
-                      </Route>
-                      <Route path="/table-assert">
-                          <TableAssert />
-                      </Route>
-                  </Switch>
-                </header>
-                  <footer>
-                      Created by : Vinayak Yuvraj Bhatt (Software Developer and JS enthusiast)
-                                    Habilelabs.io
-                  </footer>
-              </div>
-            </Router>
-          </div>
-      );
-  }
+    render() {
+        return (
+            <div className="wrapper">
+                <Router>
+                    <div className="App">
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/home">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/log">Log</Link>
+                                </li>
+                                <li>
+                                    <Link to="/table-assert">Table|Assert</Link>
+                                </li>
+                                <li>
+                                    <Link to="/time">Time|TimeEnd</Link>
+                                </li>
+                                <li>
+                                    <Link to="/todoclass">Todo</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                        <header className="App-header">
+                            <Presentation/>
+                            <Switch>
+                                <Route path="/home">
+                                </Route>
+                                <Route path="/" exact={true}>
+                                </Route>
+                                <Route path="/log">
+                                    <Log/>
+                                </Route>
+                                <Route path="/table-assert">
+                                    <TableAssert/>
+                                </Route>
+                                <Route exact={true} path="/time">
+                                    <Time/>
+                                </Route>
+                            </Switch>
+                        </header>
+                        <footer>
+                            Created by : Vinayak Yuvraj Bhatt (Software Developer and JS enthusiast)
+                            Habilelabs.io
+                        </footer>
+                    </div>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/time.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export default class time extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: undefined,
+            total: 0
+        };
+        this.handleInput = this.handleInput.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleInput(e) {
+        this.setState({value: e.target.value})
+    }
+
+    handleClick(e) {
+        console.log('time started')
+        console.time('for');
+        let sum = 0;
+        for (let i = 1; i <= this.state.value; i++) {
+            if (i === this.state.value / 2) {
+                console.timeLog('for', 'test');
+            }
+            sum += i;
+        }
+        this.setState({total: sum})
+        console.timeEnd('for');
+        console.log('time ended')
+
+    }
+
+    render() {
+        let displaySum = this.state.total;
+        return (
+            <React.Fragment>
+                <h1>For loop to </h1>
+                <input onChange={this.handleInput}/>
+                <button onClick={this.handleClick}>run loop</button>
+                sum = {displaySum}
+            </React.Fragment>
+        );
+    }
+}
